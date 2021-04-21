6b196ec7575122863559ae6a3dcb408bf4602393
mulben <82270525+mulben@users.noreply.github.com>
2021-04-21 20:49:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='m/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -11,12 +11,13 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-async def on_ready():
-    await bot.change_presence(activity=discord.Game(f"Herokuを")
 
-@bot.command()
+@bot.command(name='ping')
 async def ping(ctx):
     await ctx.send('pong')
+@bot.command(name='ネタ')
+async def _neta(ctx):
+    await ctx.reply('あたいったら最強ね')
 
 
 
