c7eb8b05a48ec611641a55f81f5ed9b544516f87
mulben <82270525+mulben@users.noreply.github.com>
2021-04-24 14:53:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,8 +35,8 @@ async def site(ctx):
     await ctx.send('http://yugiri.starfree.jp/index.html')
     
 
-@bot.command(name='sos')
-async def sos(ctx):
+@bot.command(name='緊急')
+async def _sos(ctx):
     await ctx.send('@MCO2.sys#8200','@MCO2.sys#8200','@MCO2.sys#8200')
     
 
@@ -68,7 +68,7 @@ async def info(ctx):
     embed.add_field(name="```公式サイト```",value="音楽と絵文字の部屋の公式サイトのリンクが表示されます。未完成です。",inline=False)
     embed.add_field(name="```ui```",value="Botの概要が見れます。",inline=False)
     embed.add_field(name="```info```",value="困ったときに使うと、これが表示されます。",inline=False)
-    embed.add_field(name="```sos```",value="緊急時以外使用禁止。これを使用すると私にメンション爆弾が投下されます。",inline=False)
+    embed.add_field(name="```緊急```",value="緊急時以外使用禁止。これを使用すると私にメンション爆弾が投下されます。",inline=False)
     embed.set_footer(text="made by mco2.sys #8200")
     await ctx.send(embed=embed)
 
